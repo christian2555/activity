7672a9da62d832b1ff64e3234cf1b9ab12ecb305
christian2555 <07311595@dwc-legazpi.edu>
2024-02-01 11:32:17 +0800

    initial commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello
+{
+  /*
+   * Author: Christian Q. Breta
+   * Date: January, 25, 2024
+   * Project Activity I - Hello World
+   * 
+   * note: Do not delete this, Thank you!
+  */
+    public static void main(String[] args)
+    {
+      System.out.println("Hello World!");
+    }
+}
